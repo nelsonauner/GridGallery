90f1520662841d34178a41036beda7dc7aa35832
Nelson Auner <nelson@nelsonauner.com>
2015-04-25 17:17:58 -0500

    regex expression to pull out date from comment is a go

--- R/master.R
+++ R/master.R
@@ -1,41 +1,84 @@
-# should point to R/
-baseDir <- getwd()
-# find names of scripts placed in YourCodeHere
-substrRight <- function(x, n=2){substr(x, nchar(x)-n+1, nchar(x))}
-scripts <- dir("./YourCodeHere/")
-print(scripts)
+makeGraphic <- function(code) {
+  # input: code (right now, path to file) that produces a graph
+  # output: none
+  # effect: print a file to name.png
+  png(file=paste0("../img/thumb/",code,".png"),width = 300,height=200,res=25)
+  #consider using:
+  par(mar=c(5,3,2,2)+0.1)
+  setwd("./YourCodeHere/") #you have to do this so that the scripts can access local assets... 
+  source(code)
+  #TODO: we'll use this later: 
+  #scriptContents <- readChar(currentScript, file.info(currentScript)$size)
+  #print(scriptContents)
+  setwd("..")
+  dev.off()
+  #file.copy(from=currentScript,to=paste0("./processedCode/",scripts[i]))
+  #file.remove(currentScript)
+  #no need to remove
+}
 
-# scripts should be executed under the following conditions:
-#     - must end in ".R"
-#     - must not have same name as another, already processed script
+isDate(character) {
+  # http://www.regexr.com/3ast2
+  # input: a character vector of an entire code file
+  # ideally 'codeText' object from makePost
+  # output: the jekyll-friendly object corresponding to the string #date: 
+  grep(pattern =".*?[#][ ]*?(?i)date(?-i):[ ]*?[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}",test)
+}
 
-alreadyPosted <- strsplit(dir("../img/thumb/"),".png",fixed=TRUE)
+makePost <- function(code) {
+# input: code (right now, path to file) that produces a graph
+# output: none
+# effect: print a file to name.png
+  setwd("./YourCodeHere/")
+codeText <- readLines(code, file.info(code)$size)
+publish_date <- findDate(codeText)
+setwd("..")
+# we literally have to produce the rgraph .markdown post here
+# starting working directory should be 
 
-# todo: 
-#     - throw an error if alreadyPosted
-#     - throw a warning for all non-R files (they won't be run)
-scripts <-  scripts[sapply(scripts,
-               FUN=function(x) {
-                 substrRight(x)==".R" && 
-                 !(strsplit(x,".R") %in% alreadyPosted)
-                 })]
-
-# iterate over all valid scripts and produce graphics
-for (i in 1:length(scripts)) {
-    currentScript <- paste0("./YourCodeHere/",scripts[i])
-    print("processing")
-png(file=paste0("../img//",scripts[i],".png"),width = 400,height=400)
-scriptContents <- readChar(currentScript, file.info(currentScript)$size)
-print(scriptContents)
-setwd("./YourCodeHere/") #you have to do this so that the scripts can access local assets... 
-eval(parse(scriptContents))
-
-
-setwd("../")
-dev.off()
-    file.copy(from=currentScript,to=paste0("./processedCode/",scripts[i]))
-    #file.remove(currentScript)
-    #no need to remove
+  fileConn<-file(paste0("../_posts/",code,".markdown") )#open file connection
+writeLines(c(
+            "---",
+            "layout: post",
+            paste0("thumbnail: img/thumb/",code,".png"),
+            "---",
+            codeText
+),fileConn)
+close(fileConn)
 }
 
 
+#MAIN=======================
+# should point to R/
+
+parseCodeFiles <- function() {
+  setwd("C:/Users/n_auner/tech/GridGallery/R")
+  baseDir <- getwd()
+  # find names of scripts placed in YourCodeHere
+  substrRight <- function(x, n=2){substr(x, nchar(x)-n+1, nchar(x))}
+  scripts <- dir("./YourCodeHere/")
+  print(scripts)
+  # scripts should be executed under the following conditions:
+  #     - must end in ".R"
+  #     - must not have same name as another, already processed script
+  alreadyPosted <- strsplit(dir("../img/thumb/"),".png",fixed=TRUE)
+  # todo: 
+  #     - throw an error if alreadyPosted
+  #     - throw a warning for all non-R files (they won't be run)
+
+  scripts <-  scripts[sapply(scripts,
+                             FUN=function(x) {
+                               substrRight(x)==".R" && 
+                                 !(strsplit(x,".R") %in% alreadyPosted)
+                             })]
+  print(scripts)
+  # iterate over all valid scripts and produce graphics
+  for (i in 1:length(scripts)) {
+    currentScript <- paste0("./YourCodeHere/",scripts[i])
+    print("processing")
+    makeGraphic(scripts[i])
+    makePost(scripts[i])
+  }
+}
+
+parseCodeFiles()
